f5a24701a31d785449bfa5769c0061e78f710273
Jodl Gayatin <jodl.gayatin@gmail.com>
2016-11-04 17:44:40 +0800

    Delete Plot 1.R

--- Plot 1.R
+++
@@ -1,5 +0,0 @@
-data1<-data1[6001:11000,]
-data1set<-data1[637:3516,]
-histdata<-data$Global_active_power
-hist(histdata,col="red",nclass = 12,xlab="Global Active Power (kilowatts)", 
-     ylab="Frequency", main = "Global Active Power")
